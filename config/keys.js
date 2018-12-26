5fb9af574c185d0176efe412816a8f4a2accb14b
david <thyun.ahn@gmail.com>
2018-12-26 15:49:45 +0900

    work for login and registration validation

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://thyun.ahn:md20160222@ds115963.mlab.com:15963/mernclass"
+  mongoURI: "mongodb://thyun.ahn:md20160222@ds115963.mlab.com:15963/mernclass",
+  secretOrKey: "secret"
 };
